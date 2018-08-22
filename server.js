1942f7c2a1ccbe396f664a284b4575dbfb2025d9
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-08-22 17:57:57 +0300

    pizdec

--- server.js
+++ server.js
@@ -6,12 +6,37 @@ const	port = 3000;
 const	express = require('express'),
 		bodyParser = require('body-parser'),
 		app = express();
+var		torrentStream = require('torrent-stream'),
+		magnetLink = require('magnet-link');
+
+
 
 app.use(bodyParser.urlencoded({	extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send("hello mazafaka");
+});
+
+
+
+app.post('/stream', function (req, res) {
+	let engine = torrentStream('magnet:my-magnet-link');
+
+	engine.on('ready', function() {
+		engine.files.forEach(function(file) {
+			console.log('filename:', file.name);
+			var stream = file.createReadStream();
+			// stream is readable stream to containing the file content
+		});
+	});
+
+	// get a stream containing bytes 10-100 inclusive.
+	let stream = file.createReadStream({
+		start: 10,
+		end: 100
+	});
+
 });
 
 app.listen(port, function(){
