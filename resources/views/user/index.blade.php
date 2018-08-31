04e3eaeb89242eb0d43737c2fba185e396ab9619
Julia <julyettka@gmail.com>
2018-08-31 15:58:08 +0300

    some changes

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,7 +15,6 @@
 						<div class="row">
 							<div class="col-12">
 								<div class="img-circle" style="float: left;">
-									{{--<img src="{{ url(Auth::user()->avatar) }}" class="img-circle">--}}
 									<avatar-crope img="{{ url(Auth::user()->avatar) }}" token="{{ Auth::user()->api_token  }}"></avatar-crope>
 								</div>
 								<div style="float: right;">
