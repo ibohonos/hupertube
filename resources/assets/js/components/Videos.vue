1b5504a5b5c1c6a7b76a6f0d74be4b01273e5ef1
ibohonos <ibohonos@e1r5p11.unit.ua>
2018-08-31 20:37:50 +0300

    add scroll to top

--- resources/assets/js/components/Videos.vue
+++ resources/assets/js/components/Videos.vue
@@ -70,6 +70,7 @@
 		<span slot="no-more" v-if="!videos">
 			{{ $lang.videos.no_news }}
 		</span>
+		<a href="javascript:" id="return-to-top" @click="scrollToTop"><i class="fa fa-arrow-up"></i></a>
 	</div>
 	<div class="loader mx-auto" v-else></div>
 </template>
@@ -192,17 +193,72 @@
 				}).catch(err => {
 					return false;
 				})
+			},
+
+			scrollToTop() {
+				$('body,html').animate({
+					scrollTop : 0				// Scroll to top of body
+				}, 500);
 			}
 		},
 		mounted() {
 			this.$lang.setLang(currentLang);
 			this.getAllVideos();
 			this.getAllGenres();
+			$(window).scroll(function() {
+				if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+					$('#return-to-top').fadeIn(200);    // Fade in the arrow
+				} else {
+					$('#return-to-top').fadeOut(200);   // Else fade out the arrow
+				}
+			});
 		}
 	}
 </script>
 
 <style scoped>
+
+	#return-to-top {
+		position: fixed;
+		bottom: 20px;
+		right: 20px;
+		background: rgb(0, 0, 0);
+		background: rgba(0, 0, 0, 0.7);
+		width: 50px;
+		height: 50px;
+		display: block;
+		text-decoration: none;
+		-webkit-border-radius: 35px;
+		-moz-border-radius: 35px;
+		border-radius: 35px;
+		display: none;
+		-webkit-transition: all 0.3s linear;
+		-moz-transition: all 0.3s ease;
+		-ms-transition: all 0.3s ease;
+		-o-transition: all 0.3s ease;
+		transition: all 0.3s ease;
+	}
+	#return-to-top i {
+		color: #fff;
+		margin: 0;
+		position: relative;
+		left: 16px;
+		top: 13px;
+		font-size: 19px;
+		-webkit-transition: all 0.3s ease;
+		-moz-transition: all 0.3s ease;
+		-ms-transition: all 0.3s ease;
+		-o-transition: all 0.3s ease;
+		transition: all 0.3s ease;
+	}
+	#return-to-top:hover {
+		background: rgba(0, 0, 0, 0.9);
+	}
+	#return-to-top:hover i {
+		color: #fff;
+		top: 5px;
+	}
+
 	.loader {
 		border-top: 16px solid blue;
 		border-right: 16px solid green;
